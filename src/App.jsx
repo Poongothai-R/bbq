500a63216a05c9cb0a714acabfbfbd343fb34b5d
Poongothai-R <poongothai.rajesh@icloud.com>
2023-03-10 21:58:50 +0100

    Final check and refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -10,37 +10,40 @@ import "./scripts/scroll";
 //Projects
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
+import Admin from "./pages/Admin";
 import Contact from "./pages/Contact";
 import Menu from "./pages/Menu";
-import AdminMenu from "./pages/AdminMenu";
-import Product from "./pages/Product";
 import Modal from "./components/Modal";
 import Category from "./pages/Category";
 import { useItems } from "./state/ContextItems";
+import Product from "./pages/Product";
+import Footer from "./components/Footer"
+import AdminMenu from "./pages/AdminMenu";
 import AdminCategory from "./pages/AdminCategory";
 
-export default function App() {
-  const {modal, setModal} = useItems();
-  return (
-    <BrowserRouter>
-      <ScrollTop />
-      <Navbar />
-      <Routes>
-        <Route>
-          <Route path="/" element={<Home />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/menu" element={<Menu />} />
-          <Route path="/menu/:category" element={<Category />} />
-          <Route path="/menu/:category/:productID" element={<Product />} />
 
-          <Route path="/admin/menu" element={<AdminMenu />} />
-          <Route path="/admin/menu/:category" element={< AdminCategory setModal={setModal} />} />
-          <Route path="/admin/menu/:category/:productID" element={<Product />} />
-        </Route>
-      </Routes>
-      <Modal modalState={[modal, setModal]}/>
-    </BrowserRouter>
-  );
+export default function App() {
+    const { modal, setModal } = useItems();
+    return (
+        <BrowserRouter>
+            <ScrollTop />
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/contact" element={<Contact />} />
+                <Route path="/menu" element={<Menu />} />
+                <Route path="/menu/:category" element={<Category />} />
+                <Route path="/menu/:category/:product" element={<Product />} />
+
+                <Route path="/admin" element={<Admin />} />
+                <Route path="/admin/menu" element={<AdminMenu/>} />
+                <Route path="/admin/menu/:category" element={<AdminCategory />} />
+                <Route path="/admin/menu/:category/:product" element={<Product />} />
+            </Routes>
+            <Footer/>
+            <Modal modalState={[modal, setModal]} />
+        </BrowserRouter>
+    );
 }
 
 
