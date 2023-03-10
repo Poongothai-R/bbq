92b046e714169729f71b69ea4af3d09c8e8dceb2
Poongothai-R <poongothai.rajesh@icloud.com>
2023-03-10 20:52:44 +0100

    Final check and refactoring

--- src/components/ModalAddForm.jsx
+++ src/components/ModalAddForm.jsx
@@ -2,8 +2,7 @@ import { useState } from "react";
 import InputField from "./InputField";
 import form from "../data/formFields.json";
 import { useItems } from "../state/ContextItems";
-import { ImageProcess } from "../scripts/imageProcess.js";
-import { onCreate } from "../scripts/dataManipulation";
+import { DataProcess } from "../scripts/DataProcess";
 
 export default function ModalAddForm({ path }) {
     const { setModal, menuData, setMenuData, productData, setProductData } = useItems();
@@ -11,58 +10,47 @@ export default function ModalAddForm({ path }) {
     const [description, setDescription] = useState("");
     const [imageURL, setImageURL] = useState("");
     const [price, setPrice] = useState("");
-    const [ingredients, setIngredients] = useState([]);
-    let folderName = (path === "menu") ? "category-image" : "product-image";
-    let newItem = {};
+    const [ingredients, setIngredients] = useState("");
+    let globalData = (path === "menu") ? menuData : productData;
 
     async function onSubmit(event) {
         event.preventDefault();
-        let image ;
-        if (imageURL !== undefined) {
-            image = await ImageProcess(imageURL, folderName);
+        const formData = {
+            "name": name,
+            "description": description,
+            "imageURL": imageURL,
+            "price": price,
+            "ingredients": ingredients.split(','),
         }
-        else{ image = imageURL }
-        if (path === "menu") {
-            newItem = {
-                "name": name,
-                "description": description,
-                "imageURL": image,
-            };
-            const data = await onCreate(newItem, menuData, path);
-            setMenuData(data);
-        }
-        else {
-            newItem = {
-                "name": name,
-                "description": description,
-                "imageURL": image,
-                "price": price,
-                "ingredients": ingredients,
-            };
-            const data = await onCreate(newItem, productData, path);
-            setProductData(data);
-        }
+        const result = await DataProcess(null, path, formData, globalData);
+        if (path === "menu") { setMenuData(result) }
+        else { setProductData(result) }
         resetForm();
     }
+
     function resetForm() {
         setName("");
         setDescription("");
         setImageURL("");
+        setPrice("");
+        setIngredients("");
         setModal(null);
     }
 
     return (
-        <form className="modal_form" onSubmit={(event) => onSubmit(event)}>
-            <h2>Create New item</h2>
-            <InputField settings={form.name} state={[name, setName]}/>
-            <InputField settings={form.description} state={[description, setDescription]}/>
-            <InputField settings={form.addImageURL} state={[imageURL, setImageURL]}/>
+        <form className="modal-form" onSubmit={(event) => onSubmit(event)}>
+            <h1>Create New item</h1>
+            <InputField settings={form.name} state={[name, setName]} />
+            <InputField settings={form.description} state={[description, setDescription]} />
+            <InputField settings={form.addImageURL} state={[imageURL, setImageURL]} />
             {path !== "menu" && <>
-                <InputField settings={form.price} state={[price, setPrice]}/>
-                <InputField settings={form.ingredients} state={[ingredients, setIngredients]}/></>
-            }
-            <button className="button_primary">Submit</button>
-            <button className="button_secondary" onClick={resetForm}>Cancel</button>
+                <InputField settings={form.price} state={[price, setPrice]} />
+                <InputField settings={form.ingredients} state={[ingredients, setIngredients]} />
+            </>}
+            <div className="form-button">
+                <button className="button-primary" >Submit</button>
+                <button className="button-secondary" onClick={resetForm}> Cancel </button>
+            </div>
         </form>
     );
 }
