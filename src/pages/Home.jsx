753f6edf441561a054006123dcec0675234befd2
Poongothai-R <poongothai.rajesh@icloud.com>
2023-03-10 20:36:48 +0100

    Final check and refactoring

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,20 @@
-import HeroImage from "../assets/hero/bbq_hero.jpg";
+import DesktopHeroImage from "../assets/hero/bbq-desktop.jpg";
+import MobileHeroImage from "../assets/hero/bbq-mobile.jpg";
+import TabHeroImage from "../assets/hero/bbq-tab.jpg";
+
+export default function Home() {
 
 
-export default function Home(){
-    return(
-        <div id="home">
-            <img src={HeroImage} alt="Meat and steak food" />
+    return (
+        <div id="home" className="home-page">
+            <picture>
+                <source media="(min-width:960px)" srcSet={DesktopHeroImage} />
+                <source media="(min-width:650px) " srcSet={TabHeroImage} />
+                <img src={MobileHeroImage} alt="Meat and steak food" />
+            </picture>
+            <div className="hero-content">
+                <h1 >Vegetarian is a word invented by someone who could not hunt!!</h1>
+            </div>
         </div>
     );
 }
